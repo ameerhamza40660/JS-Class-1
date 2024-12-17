d76ce00f7984a748220e3f613bdc93dc378096e3
AmeerHamza <ameerhamza123456789125@gmail.com>
2024-12-17 21:15:29 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+//CHAPTER-1//   
+//Question-1//
+alert("HELLOW - WELLCOME")
+
+//Question-2//
+alert("HELLOW \n WELLCOME")
+
+
+
+//CHAPTER-2// 
+//Question-1//
+var Username = "AmeerHamza";
+alert(Username)
+
+//Question-2//
+var Age = "20-year-old";
+alert(Age)
+
+//Question-3//
+alert("Certified Mobile Application Developer")
+
+//Question-4//
+alert(" pizza\n pizz\n piz\n pi\n p")
+
+//Question-5//
+alert(" My Email Address is ameerhamza123456789125@gmail.com")
+
+//Question-6//
+alert("I am trying to learn from  the Book  A smarter \n way to learn JavaScript")
+
+//Question-7//
+document.write(" YAh! I can  write  HTML content through JavaScript")
+
+//Question-8//
+alert(" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ")
+
+
+
+
+//CHAPTER-3//   
+//Question-1//
+
+var userName = prompt('Enter Your Name ', "Name:")
+  / console.log(userName)
+
+var passWord = +prompt('Enter Your PAsswond', "Password:")
+console.log(passWord)
+
+
+var city = prompt("Enter Your city");
+
+
+if (city == "karachi") {
+  console.log("Paskistan Zindabad")
+
+}
+else {
+  console.log('Sorry, We do not have any city in our database')
+}
+
